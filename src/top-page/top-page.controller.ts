312193638f8321f64d6c8f793d2c1c85c504509c
Denis Zhibenkov <dzhibenkov@lachestry.com>
2023-02-18 15:32:32 +0300

    Connect models described earlier

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 import { FindTopPageDto } from './dto/find-top-page.dto';
 import { TopPageModel } from './top-page.model';
-import { ConfigService } from '@nestjs/config';
 
 @Controller('top-page')
 export class TopPageController {
